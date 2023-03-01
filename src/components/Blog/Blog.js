c2b0550cc3fdfecdbe7020cc4735217fb04796ea
Spandan Roy <spandanr@zeta.tech>
2023-03-01 16:13:28 +0530

    add blog list section and fix some css issues

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -6,13 +6,20 @@ const Blog = () => {
     return (
         <div className={styles.blog}>
             <div className={styles.headerImage}></div>
-            <div className={styles.blogList}>{
-                Blogs.map(blog => {
-                    return (
-                        <div>This is a card</div>
-                    )
-                })
-            }</div>
+            <div className={styles.blogList}>
+                <div className={styles.blogHeader}>Posts</div>
+                {
+                    Blogs.map(({ title, description }) => {
+                        return (
+                            <div className={styles.blogCard}>
+                                <div className={styles.title}>{title}</div>
+                                <div className={styles.line}></div>
+                                <div className={styles.description}>{description}</div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 };
